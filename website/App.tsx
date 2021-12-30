39bb1387370ffaafa279e90273de31b1f2775ff0
shanejix <shanejix@hotmail.com>
2021-12-30 17:49:18 +0800

    feat: render data

--- website/App.tsx
+++ website/App.tsx
@@ -1,5 +1,61 @@
 import React from 'react';
+import { NAMESPACES, NAMESPACE_CONFIG, Namespace, Rule } from '../config';
 
 export default function App() {
-  return <div>hallo react</div>;
+
+  return (
+    <>
+      {
+        Object.values<Rule>(NAMESPACE_CONFIG['base'].ruleConfig).map(
+          (
+            {
+              name,
+              value,
+              description,
+              reason,
+              badExample,
+              goodExample,
+              fixable,
+              extendsBaseRule,
+              requiresTypeChecking,
+            }
+          ) => (
+            <div
+              key={name}
+            >
+              name:{name}
+              value:{JSON.stringify(value)}
+              description:{description}
+              reason:{reason}
+              fixable:{fixable}
+              extendsBaseRule:{extendsBaseRule}
+              requiresTypeChecking:{requiresTypeChecking}
+              {
+                badExample && (
+                  <pre>
+                    <code
+                      dangerouslySetInnerHTML={{
+                        __html: badExample,
+                      }}
+                    />
+                  </pre>
+                )
+              }
+              {
+                goodExample && (
+                  <pre>
+                    <code
+                      dangerouslySetInnerHTML={{
+                        __html: goodExample,
+                      }}
+                    />
+                  </pre>
+                )
+              }
+            </div>
+          )
+        )
+      }
+    </>
+  );
 }
